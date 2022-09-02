7a39a4e90f8d907cf267bfeff670526ff7c4669d
Deevanshi Sharma <87795451+DeevanshiSharma@users.noreply.github.com>
2022-09-02 17:08:21 +0530

    update

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from selenium import webdriver
 
-
 def google_search():  
     # input from user
-    search_string = input("What is the string or URL you want to search ?  ")
-  
+    st = input("Enter string/URL- ")
     # structuring the input for search
-    search_string = search_string.replace(' ', '+') 
+    search_string = st.replace(' ', '+') 
   
     # Assigning the browser variable with chromedriver of google chrome
     chrome_browser = webdriver.Chrome('chromedriver')
   
     # browsing the user input
-    for i in range(1):
-        search_input = chrome_browser.get("https://www.google.com/search?q=" +
-                                        search_string + "&start=" + str(i))
+    
+    search_input = chrome_browser.get("https://www.google.com/search?q=" +
+                                    search_string + "&start=" + 1)
 
 
-# calling the google_search function
-while True:
+if __name__ == "__main__":
     google_search()
